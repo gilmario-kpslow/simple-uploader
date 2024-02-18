0a5977af45988b2d7c17a7a0351bf8d338db149a
Gilmario <gilmariosoftware@gmail.com>
2024-02-18 10:57:54 -0300

    Comit automatico gerado pelo sistema

--- build-docker.sh
+++ build-docker.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-DOCKER_BUILDKIT=1 docker build -t gilmariokpslow/uploader-api:v1.1.0 .
+DOCKER_BUILDKIT=1 docker build -t gilmariokpslow/uploader-api:v1.2.0 .
 
 DOCKER_BUILDKIT=1 docker build -t gilmariokpslow/uploader-api:latest .
--- push.sh
+++ push.sh
@@ -4,4 +4,4 @@ docker login
 
 docker push gilmariokpslow/uploader-api:latest
 
-docker push gilmariokpslow/uploader-api:v1.1.0
+docker push gilmariokpslow/uploader-api:v1.2.0
